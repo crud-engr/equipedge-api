92fad2be8eee1e593ce77b913a6c72265a64bd3b
Abeeb <abeebayinla@gmail.com>
2022-08-21 23:27:56 +0100

    added admin routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { Express, Request, Response, NextFunction } from 'express';
 import IndexRoute from './index.routes';
 import HealthcheckRoute from './healthcheck.routes';
-import UserRoute from './user/user.routes';
+import AdminRoute from './admin/user-mgt.routes';
 import RoleRoute from './role.routes';
 
 export default function (app: Express) {
@@ -20,5 +20,8 @@ export default function (app: Express) {
     app.use('/api/roles', RoleRoute);
 
     // user
-    app.use('/api/users', UserRoute);
+    // app.use('/api/users', UserRoute);
+
+    // admin
+    app.use('/api/admin/users', AdminRoute)
 }
